ab4163aa11f2b8650d2bc68ef5d7c8d90d4279ee
Sean Du <sean.du@bhnetwork.com>
2016-02-03 14:29:58 -0800

    add profiler and multi-thread update logic

--- src/main/java/com/cex/ignite/client/DemoMain.java
+++ src/main/java/com/cex/ignite/client/DemoMain.java
@@ -3,9 +3,15 @@ package com.cex.ignite.client;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.lang.IgniteCallable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.profiler.Profiler;
 
 import com.cex.ignite.model.Transaction;
 import com.cex.ignite.model.TransactionKey;
@@ -13,45 +19,110 @@ import com.cex.ignite.service.IgniteCacheService;
 import com.cex.ignite.service.ProcessingService;
 
 public class DemoMain {
-
-	public static void main(String[] args) {
-		IgniteCacheService service = IgniteCacheService.instance;
-		service.initialize();
-		igniteCrud(service);
-		remoteProcessing(service);
-		service.finish();  
+	
+	private static Logger logger = LoggerFactory.getLogger(DemoMain.class);
+	
+	static AtomicInteger counter = new AtomicInteger(0);
+	
+	static Profiler p = new Profiler("Demo");
+	
+	static{
+		p.setLogger(logger);
 	}
 
-	private static void igniteCrud(IgniteCacheService service) {
+	public static void main(String[] args) {
+		
+		logger.info("start demo...");
+		int size = 10000;
+		IgniteCacheService service = IgniteCacheService.instance;
+		service.initialize();
+		remoteProcessing(service);
+		populateDate(service, size);
+		readData(service, size);
+		updateData(service, size);
+		service.finish();  
+		
+		p.log();
+	}
+
+	private static void populateDate(IgniteCacheService service, int size) {
 		IgniteCache<TransactionKey, Transaction> cache = service.getTxnCache();
 		
-		long t1 = System.currentTimeMillis();
-		for (int i = 1; i <= 10020; i++) {
+		logger.info("start populateData...");
+		p.start("remove cache");
+		cache.removeAll();
+		p.start("write");
+		for (int i = 0; i < size; i++) {
 			TransactionKey key = new TransactionKey(Integer.toString(i));
 			Transaction t = new Transaction(Integer.toString(i), "adfasdfasdfasdf", "channel-1560", "upc-1233534", 
 					100.50, "ACT", new Timestamp(System.currentTimeMillis()), "comments "+i);
 			cache.put(key, t);
 		}
-		long t2 = System.currentTimeMillis();
-		long t3 = (t2-t1)/1000;
+		p.start("write end");
 		
-		System.out.println("time used write= "+t3+"seconds");
-		
-		
-		Transaction txn2 = null;
-		t1 = System.currentTimeMillis();
-		for(int k = 1; k<9999; k++){
-			txn2 = cache.get(new TransactionKey(Integer.toString(k)));
-			if(k==0){
-				System.out.println(txn2);
-			}
-		}
-		t2 = System.currentTimeMillis();
-		t3 = (t2-t1)/1000;
-		System.out.println("time used read= "+t3+"seconds");
-		System.out.println("txn = "+txn2.toString());
 	}
 	
+	private static void readData(IgniteCacheService service, int size) {
+		IgniteCache<TransactionKey, Transaction> cache = service.getTxnCache();
+		
+		logger.info("start readData...");
+		Transaction txn2 = null;
+		p.start("read cache");
+		for(int k = size; k>0; k--){
+			txn2 = cache.get(new TransactionKey(Integer.toString(k)));
+		}
+		logger.info("txn = "+txn2.toString());
+		p.start("read end");
+	}
+	
+	private static void updateData(IgniteCacheService service, int size) {
+		IgniteCache<TransactionKey, Transaction> cache = service.getTxnCache();
+
+		logger.info("start updateData...");
+		p.start("random update");
+		ExecutorService executor = Executors.newFixedThreadPool(5);
+		for (int i = 0; i < 10000; i++) {
+			Runnable worker = new WorkerThread(cache, size);
+			executor.execute(worker);
+		}
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		p.start("update end");
+
+	}
+	
+	public static class WorkerThread implements Runnable{
+		
+		IgniteCache<TransactionKey, Transaction> cache = null;
+		int n = 0;
+		
+		public WorkerThread(IgniteCache<TransactionKey, Transaction> cache, int n){
+			this.cache = cache;
+			this.n = n;
+		}
+
+		@Override
+		public void run() {
+			int randomNum = 0 + (int)(Math.random() * n); 
+			
+			TransactionKey key = new TransactionKey(Integer.toString(randomNum));
+			Transaction t = cache.get(key);
+			t.setTransactionTime(new Timestamp(System.currentTimeMillis()) );
+			t.setRemark("updated...");
+			cache.put(key, t);
+			counter.incrementAndGet();
+			
+		}
+		
+	}
+
 	public static void remoteProcessing(IgniteCacheService service) {
 		Collection<IgniteCallable<Integer>> calls = new ArrayList<>();
 
@@ -69,7 +140,7 @@ public class DemoMain {
 		// Add up all the results.
 		int sum = res.stream().mapToInt(Integer::intValue).sum();
 
-		System.out.println("Total number of characters is '" + sum + "'.");
+		logger.info("Total number of characters is '" + sum + "'.");
 	}
 
 }
